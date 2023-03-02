cd4f9aeccc97b321126a861dc8c775ed99618282
Piero Maddaleni <22846928+piemadd@users.noreply.github.com>
2023-03-02 16:04:46 -0600

    added back data

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const csv = require('csv-parser')
 
 let newIncidents = [];
+let months = {};
 let index = 0;
 
 console.log('starting processing')
@@ -14,11 +15,22 @@ fs.createReadStream('input.csv')
 		if (index % 1000 === 0) {
 			console.log(`processed ${index} rows`)
 		}
-		fs.writeFileSync(`./incidents/${row['Accident Number']}-d-${row['Accident Month']}-${row['Day']}-${row['Accident Year']}-${row['Reporting Railroad Code']}.json`, JSON.stringify(row));
+
+		if (!months[`${row['Accident Month']}-${row['Accident Year']}`]) months[`${row['Accident Month']}-${row['Accident Year']}`] = {};
+
+		months[`${row['Accident Month']}-${row['Accident Year']}`][`${row['Accident Number']}-d-${row['Accident Month']}-${row['Day']}-${row['Accident Year']}-${row['Reporting Railroad Code']}`] = row;
+
+		//fs.writeFileSync(`./incidents/${row['Accident Number']}-d-${row['Accident Month']}-${row['Day']}-${row['Accident Year']}-${row['Reporting Railroad Code']}.json`, JSON.stringify(row));
 		newIncidents.push(`${row['Accident Number']}-d-${row['Accident Month']}-${row['Day']}-${row['Accident Year']}-${row['Reporting Railroad Code']}`);
 		//console.log(`incident ${row['Accident Number']} added`)
 	})
 	.on('end', function () {
 		console.log('pushing new incidents keys')
 		fs.writeFileSync(`./incidentKeys.json`, JSON.stringify(newIncidents));
+
+		console.log('pushing new months')
+		Object.keys(months).forEach(month => {
+			console.log(`pushing ${month}`)
+			fs.writeFileSync(`./incidents/${month}.json`, JSON.stringify(months[month]));
+		});
 	})
